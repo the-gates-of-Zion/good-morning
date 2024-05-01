d7f6a1831d1e5ca5aca9aef028d82747cd113433
the-gates-of-Zion <the-gates-of-Zion@outlook.com>
2024-05-02 06:33:57 +0800

    delete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+document.title = 'Good morning 😊☀️'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
