71d53b7ea2f88ff2af1ae5134b41f5d2e5edfbbc
the-gates-of-Zion <the-gates-of-Zion@outlook.com>
2024-05-02 23:59:08 +0800

    pre

--- src/App.js
+++ src/App.js
@@ -1,70 +1,16 @@
 import logo from './logo.svg';
-import { 
-  SafeAreaView,
-  View,
-  Animated,
-  Easing,
-  TouchableHighlight,
-  Text
-  } from 'react'
-  import React,{useRef} from 'react';
+import './Appd.css';
 import Greeting from './components/Greetings';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rotateValueHolder: new Animated.Value(0)
-    };
-  }
-  componentDidMount = () => {
-    this.startImageRotateFunction();
-  }
-  startImageRotateFunction = () => {
-    Animated.loop(Animated.timing(this.state.rotateValueHolder, {
-      toValue: 1,
-      duration: 3000,
-      easing: Easing.linear,
-      useNativeDriver: true,
-    })).start();
-  };
-  render(){
-    return(
-        <div className="App">
-      <CssBaseline />
-      <Container >
-      <SafeAreaView>
-        <View>
-          <Animated.Image
-            src={logo} 
-            className="App-logo" 
-            alt="logo"
-            style={{
-              alignSelf:"center",
-              transform:
-                [
-                  {
-                    rotate: this.state.rotateValueHolder.interpolate(
-                        {
-                          inputRange: [0, 1],
-                          outputRange: ['0deg', '360deg'],
-                        }
-                      )
-                  }
-                ],
-            }}
-          />
-      
-        </View>
-      </SafeAreaView>
-      <Greeting/>
-      </Container>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Greeting/>
+      </header>
     </div>
-    
-    );
-  }
+  );
 }
 
-
+export default App;
