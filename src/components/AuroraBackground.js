3a84929fdde3dc03992cf80bec297225dd33650a
the-gates-of-Zion <the-gates-of-Zion@outlook.com>
2024-05-14 08:22:08 +0800

    add Aurora

---
+++ src/components/AuroraBackground.js
@@ -0,0 +1,43 @@
+// src/components/Greeting.js
+import React, { Component } from 'react';
+import '../css/Aurora.scss'
+export default class Aurora extends Component {
+
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+        meterosNum:15,
+        catch: false,
+    };
+    
+  }
+
+  meteorsCatch(){
+    this.setState({catch:true})
+    if(this.props.catchMeteor){
+      this.props.catchMeteor()
+    }
+    //alert("You got my heart!❤️")
+  }
+  
+  render() {
+    var meteors = []
+    for (var i = 0;i<this.state.meterosNum;i++){
+      meteors.push(
+        (<div key={i} className={`meteor-${i}`} onClick={this.meteorsCatch.bind(this)}></div>)
+      )
+    }
+    return (
+      <div>
+        <div className="star"></div>
+        {
+          meteors
+        }
+      
+      </div>
+    );
+  }
+}
+
+//export default Greeting;
